e6e25f31fc282cac14bd16294b95ef66461f6735
lokesh53941 <65780262+lokesh53941@users.noreply.github.com>
2020-07-06 19:08:07 +0530

    initial commit31

--- pandasExercise3.py
+++ pandasExercise3.py
@@ -2,5 +2,6 @@ import pandas
 df2=pandas.read_csv("C:\Lokesh\Repositories\pandas\pandasExercises\supermarkets.csv",header=None)
 print(df2)
 print("............................")
+#df3=df2
 #df3.columns=["ID","ADDRESS","CITY","ZIP","COUNTRY","NAME","EMPLOYEES" ]
 #print(df3)
